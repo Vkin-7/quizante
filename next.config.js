36b1ce4eff78aae58b8554c6dbcdc7c4553f4f49
Vkin <victorrealm@gmail.com>
2021-02-06 13:02:42 -0300

    locale ajusts

--- next.config.js
+++ next.config.js
@@ -2,5 +2,15 @@ module.exports = {
   i18n: {
     locales: ['en-US', 'pt-BR'],
     defaultLocale: 'pt-BR',
+    domains: [
+      {
+        domain: 'quizante.vercel.app.br',
+        defaultLocale: 'pt-BR',
+      },
+      {
+        domain: 'quizante.vercel.app.com',
+        defaultLocale: 'en-Us',
+      },
+    ],
   },
 }
